9665513736f5f617b1c5b19ff8ba8cd04de3225f
ce-Peard <cepeard.cep@gmail.com>
2024-10-25 16:48:06 +0200

    bouton contact replacé

--- wp-content/themes/oceanwp-child-theme-master/functions.php
+++ wp-content/themes/oceanwp-child-theme-master/functions.php
@@ -35,9 +35,12 @@ add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
 
 
-function contact_btn( $items, $args ) {
-	$items .= '<a href="/contact" class="contact-btn">Nous contacter</a>';
+// Ajout balises <li> autour du bouton de contact
+
+function contact_btn($items, $args)
+{
+	$items .= '<li class="menu-item custom-btn menu-item-type-post_type menu-item-object-page"><a href="/contact" class="contact-btn menu-link">Contact Us</a></li>';
 	return $items;
 }
 
-add_filter( 'wp_nav_menu_items', 'contact_btn', 10, 2 );
+add_filter('wp_nav_menu_items', 'contact_btn', 10, 2);
